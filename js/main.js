0d38fa7cd9686f840b0d9ed581d03edcb72249f1
LBWinnie <206589036@qq.com>
2022-08-02 23:17:30 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,52 +1,70 @@
-let musicList = [
-  {
-    src: "../audio/HoneycombSummer.mp3",
-    title: "蜂巢夏日",
-    author: "Crazy:B",
-    img: "../img/cb.jpg",
-  },
-  {
-    src: "../audio/LemonSquashCheers!.mp3",
-    title: "柠檬气泡水干杯!",
-    author: "Crazy:B & 2Wink",
-    img: "../img/cb2wk.jpg",
-  },
-];
+let musicList = [];
+fetch("../data.json")
+  .then((res) => res.json())
+  .then((ret) => {
+    console.log(ret);
+    musicList = ret;
+    setMusic();
+  });
 
 const $ = (selector) => document.querySelector(selector);
 
-const $pauseBtn = $(".icon-pause");
+const $playingBtn = $(".icon-pause");
 const $preBtn = $(".icon-play-left");
 const $nextBtn = $(".icon-play-right");
 const $title = $(".player .texts h3");
 const $author = $(".player .texts p");
 const $time = $(".player .time");
+const $progress = $(".player .progress");
+const $bgPic = document.getElementById("bgPic");
 
 let index = 0;
-let audioObject = new Audio();
-setMusic();
+let clock = null;
+let audioObject = $("#audio");
 
 function setMusic() {
   let curMusic = musicList[index];
   audioObject.src = curMusic.src;
   $author.innerText = curMusic.author;
   $title.innerText = curMusic.title;
-  audioObject.play();
-  audioObject.volume = 0.2;
+  $bgPic.src = curMusic.img;
+  if ($playingBtn.classList.contains("icon-playing")) {
+    audioObject.play();
+  }
+  // audioObject.play();
+  // audioObject.volume = 0.5;
 }
-
-$pauseBtn.onclick = function () {
+function secondToText(second) {
+  second = parseInt(second);
+  let min = parseInt(second / 60);
+  let sec = second % 60;
+  sec = sec < 10 ? "0" + sec : "" + sec;
+  //sec = (sec + "").length == 1 ? "0" + sec : sec;
+  return min + ":" + sec;
+  //当秒数是个位数时前面补0，例如已播放3s时显示0:03
+}
+$playingBtn.onclick = function () {
   if (this.classList.contains("icon-pause")) {
     this.classList.remove("icon-pause");
     this.classList.add("icon-playing");
     audioObject.play();
-    audioObject.volume = 0.2;
-    console.log(audioObject.duration);
-    console.log(audioObject.currentTime);
+    audioObject.volume = 0.5;
+    // console.log(audioObject.duration);
+    // console.log(audioObject.currentTime);
+    clock = setInterval(function () {
+      let curTime = audioObject.currentTime;
+      let totalTime = audioObject.duration;
+      let percent = curTime / totalTime;
+      $progress.style.width = percent * 100 + "%";
+      $time.innerText = secondToText(curTime) + " / " + secondToText(totalTime);
+      // console.log(curTime, totalTime);
+      // console.log(secondToText(curTime), secondToText(totalTime));
+    }, 1000);
   } else {
     this.classList.remove("icon-playing");
     this.classList.add("icon-pause");
     audioObject.pause();
+    clearInterval(clock);
   }
 };
 
@@ -54,21 +72,24 @@ $nextBtn.onclick = function () {
   index = ++index % musicList.length;
   curMusic = musicList[index];
   setMusic();
-  if ($pauseBtn.classList.contains("icon-pause")) {
-    $pauseBtn.classList.remove("icon-pause");
-    $pauseBtn.classList.add("icon-playing");
-  }
 };
 
 $preBtn.onclick = function () {
   index--;
   index = (index + musicList.length) % musicList.length;
   setMusic();
-  if ($pauseBtn.classList.contains("icon-pause")) {
-    $pauseBtn.classList.remove("icon-pause");
-    $pauseBtn.classList.add("icon-playing");
-  }
 };
+
+let audioElement = $("#audio");
+let canvasElement = $("#canvas");
+let wave = new Wave(audioElement, canvasElement);
+
+wave.addAnimation(
+  new wave.animations.Square({
+    count: 30,
+  })
+);
+
 // let $pauseBtn = document.querySelector(".icon-pause");
 // let $$btns = document.querySelectorAll(".iconfont");
 // let $nextBtn = document.querySelector(".icon-play-right");
@@ -79,7 +100,7 @@ $preBtn.onclick = function () {
 //     $pauseBtn.classList.remove("icon-pause");
 //     $pauseBtn.classList.add("icon-playing");
 //     audioObject.play();
-//     audioObject.volume = 0.2;
+//     audioObject.volume = 0.5;
 //     console.log(audioObject.duration);
 //     console.log(audioObject.currentTime);
 //   } else {
@@ -92,5 +113,5 @@ $preBtn.onclick = function () {
 // $nextBtn.onclick = function () {
 //   audioObject.src = "../audio/LemonSquashCheers!.mp3";
 //   audioObject.play();
-//   audioObject.volume = 0.2;
+//   audioObject.volume = 0.5;
 // };
